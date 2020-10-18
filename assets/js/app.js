ddb94eab030ba54097137880f26d84ba5167e6a0
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-18 17:53:43 +0000

    Add feat: card flip functionality and animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    // First we need to store all of the memory cards
+    const cards = $(".memory-card");
+
+    // flipCard function
+    $(cards).click(function() {
+        $(this).toggleClass("flip");
+    });
+
 })
