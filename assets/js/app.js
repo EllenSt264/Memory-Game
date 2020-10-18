0e477f8c14d49cc4c86509d34371a805ca5b76d7
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-18 19:42:00 +0000

    Add card match functionality to JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,36 @@ document.addEventListener("DOMContentLoaded", function() {
     const cards = $(".memory-card");
 
     // flipCard function
-    $(cards).click(function() {
-        $(this).toggleClass("flip");
-    });
 
+    let hasFlippedCard = false;
+    let firstCard, secondCard;
+    
+    $(cards).click(function() {
+        $(this).addClass("flip");
+
+        if (!hasFlippedCard) {      // if a card has not been flipped/clicked
+            // first click
+            hasFlippedCard = true;
+            firstCard = this;
+        }
+        else {
+            // second click
+            hasFlippedCard = false;
+            secondCard = this;
+
+            // Do the cards match?
+            if (firstCard.dataset.cardname === secondCard.dataset.cardname) {
+                // It's a match!
+                $(firstCard).off("click");
+                $(secondCard).off("click");
+            }
+            else {
+                // It's not a match
+                setTimeout(function(){
+                    $(firstCard).removeClass("flip");
+                    $(secondCard).removeClass("flip");
+                }, 1000);
+            }
+        }
+    });
 })
